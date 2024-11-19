8e9d8095fd8c749dfebcbca4d93d781151d92dfa
TJ <TJ@DESKTOP-8FSFFJK>
2024-11-19 13:09:52 +0900

    11/19 sql

--- src/main/java/com/fullstack/springboot/repository/MemoRepository.java
+++ src/main/java/com/fullstack/springboot/repository/MemoRepository.java
@@ -1,8 +1,17 @@
 package com.fullstack.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fullstack.springboot.entity.Memo;
+
+import jakarta.transaction.Transactional;
 
 /*
  * Repository : hibernate 를 구현한 인터페이스...
@@ -20,4 +29,53 @@ import com.fullstack.springboot.entity.Memo;
 //JpaRepository<T, ID> => T 는 CRUD 할 Entity 명, ID 는 Entity 의 PK 타입을 지정합니다.
 public interface MemoRepository extends JpaRepository<Memo, Long>{
 
+	//쿼리 메서드는 여기에서 정의합니다.
+	List<Memo> findByMnoBetweenOrderByMnoDesc(Long start, Long end);
+
+	//Pageable 을 파라미터로 주면서 Page 객체를 얻어내는 조합도 만들 수 있음..
+	Page<Memo> findByMnoBetween(Long start, Long end, Pageable pageable);
+
+	//쿼리메서드를 이용한 조건부 삭제 처리..
+	void deleteMemoByMnoLessThan(Long mno);
+	
+	
+	//@Query() : 실제 쿼리를 작성해서 수행하는 어노테이션..
+	//아마 가장 많이 사용하지 않을까 함...
+	//이 쿼리를 작성할 때 가장 주의 해야할 것은 반드시 대상이 테이블이 아닌 Entity 여야하고,
+	//엔티티의 Alias 를 반드시 from 절에 줘야 함. 
+	//객체지향의 원리를 관계형 DB 에 매핑한 쿼리임을 잊으면 안됨.
+	
+	@Query( value = "Select m from Memo m")
+	List<Memo> getMemoList();
+	
+	//이번엔 동적 파라미터 바인딩 하는 방법..
+	//?1,?2 등을 사용하는 방법... : xxx 와 같이 파람이름을 사용하는 방법, :#{} 같이 자바 빈객체처럼 사용하는 법
+	//중 편한대로 사용하길...
+	//update Memo m set m.memoText = :memoText where mno = :mno
+	//메서드는 위 쿼리에 동적 파라미터를 받아서 매핑하도록 @Param("변수명") Long mno 을 사용해서 순서대로 매핑시킴..
+	//DML을 사용할 때는 반드시 @Transactional 과 @Modifying 을 선언 해줘야 함.
+	@Transactional
+	@Modifying
+	@Query(value = "Update Memo m Set m.memoText =:memoText where m.mno = :mno")
+	int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+	
+	@Transactional
+	@Modifying
+	@Query(value = "Update Memo m Set m.memoText =:#{#param.memoText} where m.mno = :#{#param.mno}")
+	int updateMemoTextMemoObj(@Param("param") Memo memo);
+	
+	
+	//@Query 사용시에 Page 객체를 리턴받을 수 있는데, 당연히 Pageable 객체를 파라미터로 주면 됨.
+	//단!!!!!!! Pageable 이 페이지수를 계산 하려면, 반드시 조회되는 목록수(count) 가 필요함.
+	//때문에 이 쿼리를 반드시 수행해서 목록수를 같이 넘겨야 함.
+	//이 쿼리는 countQuery 라는 속성값으로 사용하면됨.
+	@Query(value = "Select m from Memo m where m.mno > :mno", countQuery = "Select count(m) from Memo m where m.mno > :mno")
+	Page<Memo> getListWithPage(@Param("mno")Long mno, Pageable pageable);
+	
+	//@Query native=true : 말 그대로, 실제 테이블을 대상으로 쿼리를 날림
+	//우리가 사용하는 쿼리문을 그대로 사용할 수 있음.
+	@Query(value = "Select * from tbl_memo where mno>0",nativeQuery = true)
+	List<Memo> getNativeList();
+	
+	
 }
--- src/test/java/com/fullstack/springboot/JpaExamApplicationTests.java
+++ src/test/java/com/fullstack/springboot/JpaExamApplicationTests.java
@@ -1,11 +1,17 @@
 package com.fullstack.springboot;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 
 import com.fullstack.springboot.entity.Memo;
 import com.fullstack.springboot.repository.MemoRepository;
@@ -31,21 +37,21 @@ class JpaExamApplicationTests {
 //		}); //stream 내부에 int 값 추출.
 //	}
 	
-	@Transactional
+	//@Transactional
 	//@Test
-	void sell() { //Memo 엔티티에서 특정 key 를 기준으로 select 해보기
-		Long mno = 100L;//찾을 키
-		
-		Memo memo = memoRepository.getReferenceById(mno); //사용하려면 반드시 transactional 을 선언해야함 
-		System.out.println(memo);
+//	void sell() { //Memo 엔티티에서 특정 key 를 기준으로 select 해보기
+//		Long mno = 100L;//찾을 키
+//		
+//		Memo memo = memoRepository.getReferenceById(mno); //사용하려면 반드시 transactional 을 선언해야함 
+//		System.out.println(memo);
 		
 //		Optional<Memo> optional = memoRepository.findById(mno);
 //		if(optional.isPresent()) { //결과가 존재 한다면...
 //			Memo memo = optional.get();
 //			System.out.println(memo);
 //		}
-		
-	}
+//		
+//	}
 	
 	//이번엔 update 작업
 	
@@ -56,10 +62,102 @@ class JpaExamApplicationTests {
 //		System.out.println(memoRepository.save(memo));
 //	}
 	
+//	@Test
+//	void delEntity() {
+//		Long mno = 100L;
+//		memoRepository.deleteById(mno);
+//	}
+	
+	//Pagination 처리용 유닛 테스트..
+//	@Test
+//	void testPageDefault() {
+//		//JPA 의 findAll(Pageable) 을 이용하는데, Pageable 객체는 PageRequest 객체를 통해 얻어냄.
+//		//이때, 필요에 따라서 Sort 객체를 생성 후 Pageable 객체 get 시 사용할 수 있음.
+//		
+//		//1페이지와, 페이지당 10개의 글 목록수 get
+//		
+//		Pageable pageable = PageRequest.of(0, 10);
+//		
+//		Page<Memo> page = memoRepository.findAll(pageable);
+//		System.out.println(page);
+//		
+//		//Page 객체를 얻어냈으니, 페이지에 대한 정보등을 메서드를 통해 get 가능함.
+//		System.out.println("---------------------------------");
+//		System.out.println("Total Page : "+ page.getTotalPages());
+//		System.out.println("총 Row 수 : " + page.getTotalElements());
+//		System.out.println("현재 페이지 No : " + page.getNumber());
+//		System.out.println("페이지당 목록 갯수 : " + page.getSize());
+//		System.out.println("다음 페이지 존재 여부" + page.hasNext());
+//		System.out.println("시작 페이지 여부 (0 Page) : " + page.isFirst());
+//		
+//		//가져온 목록 Entity 를 get 하고 , Entity 의 내용을 출력...
+//		//getContent() 를 이용하면 됨.
+//		page.getContent().forEach(ele -> System.out.println(ele.getMno() + ele.getMemoText()));
+//	}
+	
+//	@Test
+//	void testSorting() {
+//		//mno desc Sort 객체 생성
+//		Sort desSort = Sort.by("mno").descending();
+//		Pageable pageable = PageRequest.of(1, 10, desSort);
+//		Page<Memo> page = memoRepository.findAll(pageable);
+//		
+//		System.out.println("---------여긴 1 페이지로 desc 정렬된 목록들 -----------");
+//		page.getContent().forEach(ele -> System.out.println(ele.getMno() + ele.getMemoText()));
+//	}
+	
+	//JPARepositoy 의 query 메서드는 일반적인 CRUD 는 가능하나, 조건식이나, like 같은 연산자를 처리하는 메서드가 없음.
+	//이를 위해서 쿼리메서드(QueryMethod) 나 @Query("실제 query") 를 이용해서 처리할 수 있음.
+	
+	//쿼리메서드는 메서드 이름 자체가 쿼리의 구문으로 처리되는 스프링의 기능...
+	//나중에 배우겠지만 QueryDSL 등의 동적 쿼리 lib 를 사용하면, 더 확장된 기능의 쿼리를 수행할 수 있음.
+	
+	//쿼리 메서드를 이용한 조건절 처리..
+//	@Test
+//	void testQueryMethod() {
+//		System.out.println("----------메서드 쿼리 실행결과------------");
+//		memoRepository.findByMnoBetweenOrderByMnoDesc(10L, 20L).forEach(t->System.out.println(t));;
+//	
+//		//Sort 객체를 정의한 Pageable 객체를 주면서 Sorting 된 Page 객체를 쿼리메서드를 통해 get..
+//		System.out.println("-----------페이지객체----------------");
+//		Page<Memo> page = memoRepository.findByMnoBetween(10L, 50L, PageRequest.of(0, 10, Sort.by("mno").descending()));
+//		page.forEach(ele->System.out.println(ele.getMno()));
+//	}
+	
+//	@Test
+//	@Commit //update와 select 제외한 얘들은 @commit 이랑 @transactional 같이 써줘야 함!
+//	@Transactional //Select 와 Delete 가 동시에 처리되는 쿼리등에는 반드시 이 어노테이션이 들어가야 각각의 작업을 이어서 수행함.
+//	void testDelByQueryMethod() {
+//		System.out.println("---------delete 처리-----------");
+//		memoRepository.deleteMemoByMnoLessThan(10L);
+//	}
+	
+	//@Query 를 이용한 테스트.
+//	@Test
+//	void queryAnno1() {
+//		memoRepository.getMemoList().forEach(t -> System.out.println(t.getMno()));
+//	}
+	
+//	@Test
+//	void queryAnnoUpdate() {
+//		System.out.println(memoRepository.updateMemoText(99L, "이건 업데이트된 내용임"));
+//	}
+	
+//	@Test
+//	void queryAnnoUpdateMemoObj() {
+//		System.out.println(memoRepository.updateMemoTextMemoObj(Memo.builder().mno(99L).memoText("졸리냐?").build()));
+//	}
+	
+//	@Test
+//	void queryAnnoPage() {
+//		Pageable pageable = PageRequest.of(0, 10);
+//		memoRepository.getListWithPage(0L, pageable).forEach(t -> System.out.println(t));
+//	}
+
 	@Test
-	void delEntity() {
-		Long mno = 100L;
-		memoRepository.deleteById(mno);
+	void naive() {
+		System.out.println(memoRepository.getNativeList());
 	}
+	
 
 }
